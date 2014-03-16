e6ce80cbf541a495978915712e47ec5bf7203dff
lcavezza <laracavezza@gmail.com>
2014-03-16 13:26:15 -0400

    Changing the donate button more

---
+++ benslighthouse/main.js
@@ -0,0 +1,14 @@
+function parallax(){
+    var scrolled = $(window).scrollTop();
+
+    $('.bg').css('top', -(scrolled) + 'px');
+}
+
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('top', -(scrolled * 0.2) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
---
+++ benslighthouse/unused/main.js
@@ -0,0 +1,11 @@
+// Init Skrollr
+
+var s = skrollr.init({
+	forceHeight: false
+});
+
+
+// Refresh Skrollr after resizing our sections
+
+s.refresh($('.homeSlide'));
+
